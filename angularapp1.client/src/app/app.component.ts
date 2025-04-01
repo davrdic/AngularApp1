c8b3c31a996790af43807ed6b3a763c116b73925
David Dickinson <davrdic@gmail.com>
2025-04-01 11:50:36 -0500

    port 44319

--- angularapp1.client/src/app/app.component.ts
+++ angularapp1.client/src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent implements OnInit {
   }
 
   getForecasts() {
-    this.http.get<WeatherForecast[]>('https://134.122.27.167:5000/weatherforecast').subscribe(
+    this.http.get<WeatherForecast[]>('https://134.122.27.167:44319/weatherforecast').subscribe(
       (result) => {
         this.forecasts = result;
       },
