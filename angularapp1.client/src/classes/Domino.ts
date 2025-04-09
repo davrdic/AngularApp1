74275cabc64a2406a82cd9ba357657f8687a433d
David Dickinson <davrdic@gmail.com>
2025-04-09 09:24:12 -0500

    creating a game with dominos and displaying info on UI

--- angularapp1.client/src/classes/Domino.ts
+++ angularapp1.client/src/classes/Domino.ts
@@ -1,34 +1,38 @@
-export class Domino {
-  private _sideA: number;
-  private _sideB: number;
-
-  constructor(sideA: number, sideB: number) {
-    this._sideA = sideA;
-    this._sideB = sideB;
-  }
-  public set sideA(sideA: number) {
-    this._sideA = sideA;
-  }
-
-  public set sideB(sideB: number) {
-    this._sideB = sideB;
-  }
-
-  public get sideA() {
-    return this._sideA;
-  }
-
-  public get sideB() {
-     return this._sideB;
-  }
+export interface Domino {
+  sideA: number;
+  sideB: number;
 }
-const domino = new Domino(0, 0);
-export class Hand {
-  dOne: Domino;
-  dTwo: Domino;
 
-  constructor(dOne: Domino = domino, dTwo: Domino = domino) {
-    this.dOne = dOne;
-    this.dTwo = dTwo
-  }
+export type Hand = Domino[];
+
+export type Hands = Hand[]; // One hand per player
+
+export interface Arena {
+  slots: (Domino | null)[]; // Index 0 = Player One, 1 = Player Two, etc.
 }
+
+export interface ScoreCard {
+  teamScores: number[]; // [team1Score, team2Score]
+}
+
+export interface Game {
+  id: number;
+  name: string;
+  playerHands: Hands;      // e.g. [ [d1, d2, ...], [d1, d2, ...] ]
+  arena: Arena;            // arena.slots[0] = player one’s domino
+  scoreCard: ScoreCard;
+  playerTurn: number;      // index of player whose turn it is
+}
+
+export const initialGameState: Game = {
+  id: 0,
+  name: '',
+  playerHands: [],
+  arena: {
+    slots: [null, null, null, null] // Each player's played domino
+  },
+  scoreCard: {
+    teamScores: [0, 0] // Team 1 (players 1 & 3), Team 2 (players 2 & 4)
+  },
+  playerTurn: 0 // Player 1 starts
+};
